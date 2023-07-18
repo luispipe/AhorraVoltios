50bb7693849ed0aaccca1f531072aa4f5052846e
luispipe <lflondonor@unal.edu.co>
2023-07-18 12:11:26 -0300

    Third Commit

--- app/src/main/java/com/example/ahorrovoltios/LoginActivity.java
+++ app/src/main/java/com/example/ahorrovoltios/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.ahorrovoltios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login= findViewById(R.id.btLogin);
+
+        Intent home= new Intent(this,HomeActivity.class);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(home);
+            }
+        });
+
+
     }
 }
