6d0b83a2997468b2dcdabcee6a832c8871f2f5cf
luispipe <lflondonor@unal.edu.co>
2023-07-21 10:53:14 -0300

    Final Frontend Commit

---
+++ app/src/main/java/com/example/ahorrovoltios/EnergyServiceActivity.java
@@ -0,0 +1,14 @@
+package com.example.ahorrovoltios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnergyServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_energy_service);
+    }
+}
---
+++ app/src/main/java/com/example/ahorrovoltios/WaterServiceActivity.java
@@ -0,0 +1,14 @@
+package com.example.ahorrovoltios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WaterServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_service);
+    }
+}
